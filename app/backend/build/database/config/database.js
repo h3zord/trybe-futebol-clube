94d485530fc305af70b20386da0bdc351d10f698
Lucas <lucaschmain@gmail.com>
2023-02-27 23:32:57 -0300

    config railway deploy

--- app/backend/build/database/config/database.js
+++ app/backend/build/database/config/database.js
@@ -1,12 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
+// const config: Options = {
+//   username: process.env.DB_USER || 'root',
+//   password: process.env.DB_PASS || '123456',
+//   database: 'TRYBE_FUTEBOL_CLUBE',
+//   host: process.env.DB_HOST || 'localhost',
+//   port: Number(process.env.DB_PORT) || 3002,
+//   dialect: 'mysql',
+//   dialectOptions: {
+//     timezone: 'Z',
+//   },
+//   logging: false,
+// }
 const config = {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || '123456',
-    database: 'TRYBE_FUTEBOL_CLUBE',
-    host: process.env.DB_HOST || 'localhost',
-    port: Number(process.env.DB_PORT) || 3002,
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
+    port: Number(process.env.MYSQLPORT),
     dialect: 'mysql',
     dialectOptions: {
         timezone: 'Z',
