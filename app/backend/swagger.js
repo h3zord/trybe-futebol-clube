11cb7d911848d2833eb41c28e9551582fd0c770c
Lucas <lucaschmain@gmail.com>
2023-02-28 11:08:18 -0300

    config railway deploy

--- app/backend/swagger.js
+++ app/backend/swagger.js
@@ -9,7 +9,7 @@ const doc = {
   },
   host: `${process.env.MYSQLHOST}:${process.env.PORT}`,
   basePath: '/',
-  schemes: ['http'],
+  schemes: ['http', 'https'],
   consumes: ['application/json'],
   produces: ['application/json'],
   tags: [
