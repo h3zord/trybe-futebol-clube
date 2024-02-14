f6af6b14a87b1a07b802b1b9e570e096525a1294
Lucas <lucaschmain@gmail.com>
2024-02-13 21:49:33 -0300

    db: changing ssl config connection

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -1,19 +1,39 @@
 import 'dotenv/config';
 import { Options } from 'sequelize';
 
-const config: Options = {
+let config: Options
+
+process.env.PGSSL === 'true' ? config = {
   username: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
-  host: process.env.PGHOST, 
+  host: process.env.PGHOST,
   port: Number(process.env.PGPORT),
   dialect: 'postgres',
   define: {
     timestamps: true,
   },
   dialectOptions: {
-    schema: 'public'
-  }
+    schema: 'public',
+    ssl: {
+      require: true
+    }
+  },
+} : config = {
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT),
+  dialect: 'postgres',
+  define: {
+    timestamps: true,
+  },
+  dialectOptions: {
+    schema: 'public',
+  },
 }
+
+
 
 module.exports = config;
--- app/backend/src/database/models/index.ts
+++ app/backend/src/database/models/index.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from 'sequelize';
 import * as config from '../config/database';
+import 'dotenv/config'
 
 const sequelize = new Sequelize(config)
 
